59659065e3e2bcf0d678904166112d85d9ec31a6
H2WO4 <arthurlamidel@gmail.com>
2022-12-13 12:03:25 +0100

    Fix init

--- minesweeper/src/main.rs
+++ minesweeper/src/main.rs
@@ -1,20 +1,19 @@
 use yew::prelude::*;
 
 
-
 #[function_component(App)]
 fn app() -> Html {
-	html! {
-		<>
-			<div class={ "option" }>
-				<div class={ "config" }>
-				</div>
-			</div>
-		</>
-	}
+    html! {
+        <>
+            <div class={ "option" }>
+                <div class={ "config" }>
+                </div>
+            </div>
+        </>
+    }
 }
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    yew::start_app::<App>();
+    yew::Renderer::<App>::new().render();
 }
